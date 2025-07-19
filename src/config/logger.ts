560a30b5714e7eadde643ef55b05b36323d77efa
flipflop-fun <flipflopdotfun@gmail.com>
2025-07-19 15:09:58 +0800

    0.1.1

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -9,15 +9,19 @@ const logger = winston.createLogger({
   ),
   defaultMeta: { service: 'mint-price-api' },
   transports: [
-    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'logs/combined.log' }),
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.colorize(),
+        winston.format.simple()
+      )
+    })
   ],
 });
 
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
-    format: winston.format.simple()
-  }));
-}
+// if (process.env.NODE_ENV !== 'production' && process.env.VERCEL !== '1') {
+//   logger.add(new winston.transports.File({ filename: 'logs/error.log', level: 'error' }));
+//   logger.add(new winston.transports.File({ filename: 'logs/combined.log' }));
+// }
 
 export default logger;
